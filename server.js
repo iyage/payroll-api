8daca87671b1ddf1e8c17e11e00145b734cd0e3f
iyage <yahg.concept@gmail.com>
2023-08-27 21:34:30 +0100

    dotenv

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDb = require("./config/dbConnection");
 const router = require("./routes/routes");
+require("dotenv").config();
 const app = express();
 connectDb();
+
 app.use("/", router);
 mongoose.connection.once("open", () => {
   console.log("connect to db successfully");
